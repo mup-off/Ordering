3252e547cf99a6384be9da7a0e89a6ad19f3119f
MUPENZI Fulgence <mupenz250@gmail.com>
2026-03-31 10:54:57 +0200

    resolved the frontednd

--- food_ordering_project/urls.py
+++ food_ordering_project/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
- 
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "Welcome to the Food Ordering API!"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', api_root, name='home'),
 ]
